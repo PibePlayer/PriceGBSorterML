ee786c9f2c067cde8c104cb3fd63eb5784b49aa2
PibePlayer <pidal.mateo@gmail.com>
2021-10-11 16:51:30 -0300

    Update

--- PriceGBSorter.v
+++ PriceGBSorter.v
@@ -2,25 +2,46 @@ module main
 
 import net.http
 import x.json2
+import regex
+import os
+
+struct HDD {
+	name string
+	price f32
+	size int
+	permalink string
+}
 
 fn main() {
 	println('Hello World!')
-	mut raw := json2.raw_decode('{ "a": "b", "b": "c"}') ?
-	//header := http.Header{}
-	//header.add(http.CommonHeader.authorization, "Bearer test")
 	mut request := http.Request{
-		url: "https://api.mercadolibre.com/sites/MLA/search?q=Disco%20Duro",
+		url: "https://api.mercadolibre.com/sites/MLA/search?category=MLA1672",
 		method: http.Method.get
 	}
 	request.add_header(http.CommonHeader.authorization, "Bearer TG-6163150ead3e6b000762d365-134063224")
 	response := request.do() ?
-	raw = json2.raw_decode(response.text) ?
-	mapped := raw.as_map()
-	println(response.text)
 
+	query := r'([\d]+(?:\.|,[\d]{1,4})?)\s*(?P<denom>(TB)|(GB))'
+	mut re := regex.regex_opt(query) or { panic(err) }
+	println(re.get_query())
+	//re.compile_opt() ?
+
+	raw := json2.raw_decode(response.text) ?
+	mapped := raw.as_map()
+	//println(response.text)
+
+	mut str := ""
 	for product in mapped['results']?.arr() {
 		//println(mapped['results']?.arr())
-		println(product.as_map()['title']?.str())
+		str = product.as_map()['title']?.str().to_upper()
+		println(str)
+		start, end := re.find(str)
+		println(start)
+		println(end)
+		if start != -1 {
+			println(str[start..end])
+		}
 	}
+	os.write_file("/data/data/com.termux/files/home/PriceGBSorterML/test.json", response.text) ?
 	//println(response.text)
 }
